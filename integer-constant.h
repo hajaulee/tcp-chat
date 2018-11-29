64cc0a24d63749d810780d6bccf6a7afe4ee9c36
Le Cong Hau <anhlahau.hl@gmail.com>
2018-11-29 16:18:20 +0700

    Change port

--- integer-constant.h
+++ integer-constant.h
@@ -1,5 +1,5 @@
 #define MAXLINE 4096   /*max text line length*/
-#define SERV_PORT 9000 /*port*/
+#define SERV_PORT 6969 /*port*/
 #define USER_NUM_MAX 100 /*maximum number of client connections */
 
 #define CODE_ERROR_USER_NOT_FOUND -1
